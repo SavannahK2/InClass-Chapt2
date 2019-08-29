2c44ec05b86ac6552a6d7e4203d0efdfe0e466a5
0786954 <0786954@KELCE-LABS11-P.pittstate.edu>
2019-08-29 12:09:07 -0500

    Added Miles Per Gallon

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -9,5 +9,23 @@
  * @author 0786954
  */
 public class Main2 {
-    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello class this is "
+                + "a long phrase");
+        //this is how you do a comment,funNumber = 5 has no context for naming
+        int funNumber = 5;
+        String greetingPhrase = "Hello Class";
+        System.out.println(greetingPhrase + "! " + funNumber);
+        
+        float number = 25.4F;
+        //double == for comparison to see if this does equal that
+        
+        char firstLetterOfJohnsName = 'j';
+        char firstLetterOfJohnsNameInUpperCase = 'J';
+        
+        int funNum;
+        funNum =10;
+        System.out.println(greetingPhrase + " " + funNum);
+    }
 }
